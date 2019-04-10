048222260d46fca5e4cc4db8f96f74fb7f38c68b
Krish Mahajan <krish.mahajan@gmail.com>
2019-04-10 17:30:22 -0400

    Adding LinkedList DataStructure Practice Questions done on 10-04-2019

--- Java/Geek/src/main/java/datastructure/linkedlist/LinkedList.java
+++ Java/Geek/src/main/java/datastructure/linkedlist/LinkedList.java
@@ -1,248 +1,176 @@
 package datastructure.linkedlist;
 
+/**
+ * @author krish.mahajan@gmail.com
+ *
+ */
 public class LinkedList {
 
-	Node head; 
-	
-	// Basic Node class
-	static class Node {
-		int data;
-		Node next;
-		Node(int d) {
-			data = d;
-			next = null;
-		}
-		
-	}
-	
-	// Print the linkedList
-	public void printList(Node head) {
-		Node n = head;
-		while(n!=null) {
-			System.out.print(n.data + " ");
-			n = n.next;
-		}
-		System.out.println(" ");
-	} 
-	
-	// Add new node at the front of the linkedList
-	public void push(int new_data) {
-		
-		Node new_node = new Node(new_data);
-		new_node.next = this.head;
-		this.head = new_node;
-	}
-	
-	public void insertAfter(Node prev_node,int new_data) {
-		
-	 // check if the given node is not null 
-		if(prev_node ==null) {
-			System.out.println("The given previous node cannot be null");
-			return;
-		}
-		
-		Node new_node = new Node(new_data);
-		
-		new_node.next = prev_node.next;
-		prev_node.next = new_node;
-	} 
-	
-	
-	public void append(int new_data) {
-		
-		Node new_node = new Node(new_data);
-		
-		if(head==null) {
-			this.head = new Node(new_data);
-			return;
-		}
-		
-		new_node.next = null;
-		Node last = this.head;
+	private Node head; // head of list
 
-		while(last.next!=null) {
-			last = last.next;
-		}
-		
-		last.next = new_node;
-		return;
+	public Node getHead() {
+		return head;
 	}
-	
-	/* Inserts a new node at front of the list*/
-	public void deleteNode(int key) {
-		
-		// store head node
-		Node temp = head;
-		Node prev = null;
-		
-		// if head node itself holds the key to be deleted
-		if(temp !=null && temp.data == key) {
-			this.head = temp.next;
-			return;
-		}
-		
-		// Search for the key to be deleted,keep track of the 
-		//previous node as we need to change temp.next
-		while(temp!=null && temp.data!=key) {
-			prev = temp;
-			temp = temp.next;
-		}
-		
-		//If key was not found
-		if(temp==null) return;
-		
-		//Unlink the node from the linked list
-		prev.next = temp.next;
+
+	public void setHead(Node head) {
+		this.head = head;
 	}
-	
-	
-	/* Function counts no. of nodes in LinkedList iteratively
+	/**
+	 * TO check if the current node is the head of the list (Assuming LinkedList has unique entries)
+	 * @param n
+	 * @return
 	 */
-	public int getCount() {
-		Node temp = head;
-		int count = 0;
-		while(temp!=null) {
-			count++;
-			temp = temp.next;
-		}
-		return count;
+	public boolean isHead(Node n) {
+		if(n.getData() == this.getHead().getData()) return true;
+		return false;
 	}
-	
-	public int getCountRec(Node node) {
+
+	/* Linked list Node */
+	class Node {
+
+		private double data;
+		private Node next;
+
+		// Constructor to create a new node
+		// Next is by default initialized
+		Node(double d) {
+			this.data = d;
+		}
+
+		public double getData() {
+			return data;
+		}
+
+		public Node getNext() {
+			return next;
+		}
+
+		public void setNext(Node next) {
+			this.next = next;
+		}
+
+	}
+
+	public void printList() {
+		Node start = this.getHead();
+		while (start != null) {
+			System.out.print(start.getData());
+			start = start.getNext();
+			System.out.print(" ");
+		}
+	}
+
+	/**
+	 * Add a new node at the start of LinkedList
+	 * 
+	 * @param new_data
+	 */
+	public void push(int new_data) {
+		Node new_node = new Node(new_data);
+		new_node.next = this.getHead();
+		this.setHead(new_node);
+	}
+
+	/**
+	 * Add a node next to given node (cannot add new node at the start of linkedList)
+	 * @param newNodeData   Data value of new node
+	 * @param insertAfterNodeData  Data value of given node
+	 */
+	public void insertAfter(double newNodeData, double insertAfterNodeData) {
+		// create the new_node
+		Node new_node = new Node(newNodeData);
+
+		// search the existing node with given data value starting at head of the
+		// LinkedList
+		Node curr = this.getHead();
+		Node currNext = curr.next;
+
+		// iterate over the linkedList till the node is found or end of the LinkedList
+		// is reached
+		while (curr != null) {
+			if (curr.getData() == insertAfterNodeData) {
+				break;
+			}
+
+			curr = currNext;
+			currNext = curr.getNext();
+		}
+		if (curr == null) {
+			System.out.print("LinkedList has no Node with the value :" + insertAfterNodeData);
+		} else {
+			curr.setNext(new_node);
+			new_node.setNext(currNext);
+		}
+
+	}
+	/**
+	 * Delete a node in LinkedList with given data value
+	 * @param data
+	 */
+	public void deleteNode(double data) {
 		
-		//Base case
-		if(node==null) {
+		
+		//Iterate over the linkList to find the given node to be deleted
+		Node currPrev = null;
+		Node curr = this.getHead();
+		
+		
+		while(curr != null) {
+			if(curr.getData()== data) {
+				
+				//if curr is head of the linkedList
+				if(this.isHead(curr)) {
+					this.setHead(curr.getNext());
+					break;
+				}
+				//delete the node and make the new connections
+				 currPrev.setNext(curr.getNext());
+				 break;
+			}
+			
+			currPrev = curr;
+			curr = curr.getNext();
+		
+		}
+		if(curr == null) {
+			System.out.println("Key was not found");
+		}
+	} 
+	
+	
+	/**
+	 * Find size of LinkedList iteratively
+	 * @return size of LinkedList
+	 */
+	public int IterativeSize() {
+		if(this.getHead() == null) {
+			System.out.println("No linkedList found");
 			return 0;
 		}
-		
-		return 1 + getCountRec(node.next);
-		
-	} 
-	
-	public int getCountRec() {
-		return getCountRec(this.head);
+		Node curr = this.getHead();
+		int size = 0;
+		while(curr!=null) {
+			curr = curr.getNext();
+			size +=1;
+		}
+		return size;
 	}
-	
-	/*Function to swap nodes in LinkedList 
-	 * without swapping data
+	/**
+	 * Find size of LinkedList recursively
+	 * @return size of LinkedList.
 	 */
-	public void swapNodes(int x,int y)
-	{
-		
-		// Nothing to do if x and y are same
-		if(x ==y) return;
-		
-		//Search for x(keep track of prevX and CurrX)
-		Node prevX = null, currX = head;
-		while(currX!=null && currX.data !=x) {
-			prevX = currX;
-			currX = currX.next;
-		}
-		
-		//Search for y(keep track of prevY and CurrY)
-		Node prevY = null, currY=head;
-		while(currY!=null && currY.data != y) {
-			prevY =currY;
-			currY = currY.next;
-		}
-		
-		//If either x or y is not present, nothing to do
-		if(currX == null || currY == null)
-			return; 
-		
-		//If x is not head of linked list
-		if(prevX!=null) 
-			prevX.next = currY;
-		
-		else
-			head = currY;
-		
-        //If y is not head of linked list
-		if(prevY!=null) 
-			prevY.next = currX;
-		
-		else 
-			head = currX;
-		
-		
-		Node temp = currY.next;
-		currY.next = currX.next;
-		currX.next = temp;
-		
-		
+	public int RecursiveSize() {
+		Node curr = this.getHead();
+		int size = this.RecursiveSize(curr, 0);
+		return size;
 	}
-	
-	/* Function to reverse the linked list */
-	public Node reverse(Node node) {
-		Node prev = null;
-		Node curr = head;
-		Node next = null;
-		
-		while(curr !=null) {
-			next = curr.next;
-			curr.next = prev;
-			prev = curr;
-			curr = next;
-		}
-		node = prev;
-		return node;
-				
-		
-				
-	}
-	
-	/* Takes two lists sorted in  
-	increasing order, and splices  
-	their nodes together to make  
-	one big sorted list which is  
-	returned. */
-	Node sortedMerge(Node headA, Node headB) 
-	{ 
-	      
-	    /* a dummy first node to  
-	       hang the result on */
-	    Node dummyNode = new Node(0); 
-	      
-	    /* tail points to the  
-	    last result node */
-	    Node tail = dummyNode; 
-	    while(true)  
-	    { 
-	          
-	        /* if either list runs out,  
-	        use the other list */
-	        if(headA == null) 
-	        { 
-	            tail.next = headB; 
-	            break; 
-	        } 
-	        if(headB == null) 
-	        { 
-	            tail.next = headA; 
-	            break; 
-	        } 
-	          
-	        /* Compare the data of the two 
-	        lists whichever lists' data is  
-	        smaller, append it into tail and 
-	        advance the head to the next Node 
-	        */
-	        if(headA.data <= headB.data) 
-	        { 
-	            tail.next = headA; 
-	            headA = headA.next; 
-	        }  
-	        else
-	        { 
-	            tail.next = headB; 
-	            headB = headB.next; 
-	        } 
-	          
-	        /* Advance the tail */
-	        tail = tail.next; 
-	    } 
-	
-	    return dummyNode.next; 
-	} 
 
+	private int RecursiveSize(Node curr, int size) {
+		if(curr == null) {
+			return  size;
+		}
+		
+		return RecursiveSize(curr.getNext(), size +1);
+		
+	}
 }
--- Java/Geek/src/main/java/datastructure/linkedlist/LinkedListTest.java
+++ Java/Geek/src/main/java/datastructure/linkedlist/LinkedListTest.java
@@ -5,32 +5,132 @@ import datastructure.linkedlist.LinkedList.Node;
 public class LinkedListTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-        LinkedList list1 = new LinkedList(); 
-        list1.head = new Node(4); 
-        list1.head.next = new Node(15); 
-        list1.head.next.next = new Node(20); 
-        list1.head.next.next.next = new Node(84); 
-        
-        LinkedList list2 = new LinkedList(); 
-        list2.head = new Node(3); 
-        list2.head.next = new Node(16); 
-        list2.head.next.next = new Node(21); 
-        list2.head.next.next.next = new Node(80); 
-          
-        System.out.println("Given 1st Linked list"); 
-        list1.printList(list1.head); 
-        
-        System.out.println("Given 2nd Linked list"); 
-        list2.printList(list2.head); 
-       
-        System.out.println("Merging the given linkedList");
-
-        Node new_head = list1.sortedMerge(list1.head, list2.head);
- 
-        list1.head = new_head;
-        list1.printList(list1.head);
+		
+		
+		//setting the linkedlist
+		LinkedList linkedList = new LinkedList();
+		
+		Node listHead = linkedList.new Node(0);
+		Node second = linkedList.new Node(1);
+		Node third = linkedList.new Node(2);
+		Node fourth = linkedList.new Node(3);
+	   
+		linkedList.setHead(listHead);
+		linkedList.getHead().setNext(second);
+		second.setNext(third);
+		third.setNext(fourth); 
+		
+		//iterating the linkedlist
+		System.out.print("Original LinkedList " );
+		linkedList.printList();
+		System.out.println(" ");
+		
+		int size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively: " + size);	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: " + size);
+		System.out.println(" ");
+		
+		
+		//Adding new element at the start
+		System.out.print("New LinkedList after addition at the start ");
+		linkedList.push(-1);
+		linkedList.printList();
+		System.out.println(" "); 
+		 size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively: " + size);	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: " + size);
+		System.out.println(" ");
+		
+		//Adding new element  in between 1 & 2
+		System.out.print("New LinkedList after addition in between ");
+		linkedList.insertAfter(0.5, 1);
+		linkedList.printList();
+		System.out.println(" "); 
+		 size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively ");	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: " + size);
+		System.out.println(" ");
+		
+		
+		
+		//Adding new element at the last position
+		System.out.print("New LinkedList New LinkedList after addition at last position: ");
+		linkedList.insertAfter(3.5, 3);
+		linkedList.printList();
+		System.out.println(" "); 
+		 size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively: " + size);	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: " + size);
+		System.out.println(" ");
+		
+		
+		//Adding new element in between -1 & 0
+		System.out.print("New LinkedList after addition in between ");
+		linkedList.insertAfter(-0.5, -1);
+		linkedList.printList();
+		System.out.println(" "); 
+		 size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively ");	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: ");
+		System.out.println(" ");
+		
+		
+		// Deleting a element in the LinkedList
+		System.out.print("New LinkedList after deletion in between: ") ;
+		linkedList.deleteNode(2.0);
+		linkedList.printList();
+		System.out.println(" ");  
+		 size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively: " + size);	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: " + size);
+		System.out.println(" ");
+		
+		
+		
+		// Deleting a element at front of the LinkedList
+		System.out.print("New LinkedList after deletion at the beginning: " );
+		linkedList.deleteNode(-1.0);
+		linkedList.printList();
+		System.out.println(" ");  
+		 size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively: " + size);	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: " + size);
+		System.out.println(" ");
+		
+		
+		// Deleting a element at the last position of the LinkedList
+		System.out.print("New LinkedList after deletion at the last position: ");
+		linkedList.deleteNode(3.5);
+		linkedList.printList();
+		System.out.println(" "); 
+		 size =linkedList.IterativeSize();
+		System.out.print("Original LinkedList size iteratively: " + size);	
+		System.out.println(" ");
+		
+		size = linkedList.RecursiveSize();
+		System.out.print("Original LinkedList size recursively: " + size);
+		System.out.println(" ");
+		
 	}
-
 }

c259608d61f26f4e79b5c43d458efc3234359df6
krish <krish.mahajan@gmail.com>
2019-09-15 19:11:44 -0400

    Added code to check height , diameter , width ..etc of binary tree.

--- Python/Geek/binarytree/binary_tree.py
+++ Python/Geek/binarytree/binary_tree.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 from typing import List, Set, Dict, Tuple, Optional 
+import sys
 
 class Node(object):
     'A node class to represent individual Node'
-    def __init__(self, item:int)->None:
-        self.__key = item
-        self.__parent = self.__left_child = self.__right_child = Optional[Node] 
+    def __init__(self, item:Optional[int]=None)->None: 
+        if item is not None:
+            self.__key = item
+            self.__parent = self.__left_child = self.__right_child = Optional[Node] 
 
     def get_key(self)-> int:
         return self.__key
@@ -26,7 +28,7 @@ class Node(object):
         else:
             return None
 
-    def set_left_child(self,left_child_key:int)->None:
+    def set_left_child(self,left_child_key:Optional[int])->None:
         if left_child_key!=None:
             self.__left_child = Node(left_child_key)
             self.__left_child.__parent = self
@@ -39,7 +41,7 @@ class Node(object):
         else:
             return None
 
-    def set_right_child(self,right_child_key:int)->None:
+    def set_right_child(self,right_child_key:Optional[int])->None:
         if right_child_key!=None:
             self.__right_child = Node(right_child_key)
             self.__right_child.__parent = self
@@ -157,7 +159,7 @@ class BinaryTree(object):
         for element in l:
             self.insert(element) 
 
-    def inorder_successor(self,node:Optional[Node]) -> Optional[int]:
+    def inorder_successor(self,node:Optional[Node]) -> Optional[Node]:
         '''
         Find inorder successor of a node in BST
         '''
@@ -165,14 +167,14 @@ class BinaryTree(object):
             if node.get_right_child() is not None: 
                 inorder_successor_node:Optional[Node] = self.find_min_right_subtree(node.get_right_child()) 
                 if inorder_successor_node is not None: 
-                    return inorder_successor_node.get_key() 
+                    return inorder_successor_node
                 else:
                     return None
             else: 
                 if node.get_parent() is not None:
                     inorder_successor_node = self.find_max_parent_node(node.get_parent(),node) 
                     if inorder_successor_node is not None: 
-                        return inorder_successor_node.get_key() 
+                        return inorder_successor_node
                     else:
                         return None
                 else :
@@ -207,7 +209,248 @@ class BinaryTree(object):
             else:
                 return parent 
         else:
-            return node
+            return node 
+
+    def height_of_tree(self) -> int: 
+        '''
+        Function to calculate height of tree
+        '''
+        if self.get_root() is not None:
+            height :int = self._height_of_node(self.get_root()) 
+            return height
+
+        else:
+            return 0
+
+    def _height_of_node(self,node:Optional[Node])-> int:
+        '''
+        Function to calculate height of a node in Binary Tree
+        '''
+        if node is not None:
+            return ( 1 + max(self._height_of_node(node.get_left_child()),self._height_of_node(node.get_right_child())))
+        else:
+            return 0 
+
+    
+    def level_order_traversal(self)-> Tuple[Tuple[(int,int)],...]:
+        '''
+        Function to calculate level_order_traversal of Binary Tree(Breadth First Travesal)
+        '''
+        height_of_tree :int = self.height_of_tree() 
+        self.t:Tuple[Tuple[(int,int)],...] = ()
+        for level in range(height_of_tree):
+            self.print_level_order_keys(self.get_root(),1,level+1,self.t) 
+        return self.t
+        
+    def print_level_order_keys(self,node:Optional[Node],initial_level:int,target_level:int,t:Tuple[Tuple[(int,int)],...]):
+        if node is not None:
+            if(initial_level == target_level): 
+                print(target_level , '->' , node.get_key())
+                self.t += ((target_level,node.get_key()),) 
+            else:
+                self.print_level_order_keys(node.get_left_child(),initial_level+1,target_level,self.t) 
+                self.print_level_order_keys(node.get_right_child(),initial_level+1,target_level,self.t) 
+
+
+    def width_of_tree(self)-> int:
+        '''
+        Function to calculate width of Binary Tree
+        '''
+        if self.get_root() is not None:
+            map :Dict[int,int] = {}
+            map = self._width(self.get_root(),1,map) 
+            #print(map)
+            return max(map,key=map.get)
+        else:
+            return 0
+
+    def _width(self,node:Optional[Node],level:int,map:Dict[int,int])-> Dict[int,int]:
+        if node is not None: 
+            if level not in map.keys():
+                map[level] = 1
+            else:
+                map[level] +=1
+            self._width(node.get_left_child(),level+1,map)
+            self._width(node.get_right_child(),level+1,map) 
+            return map 
+        else:
+            return map  
+
+
+    def nodes_k_distance(self,k:int)->Optional[List[Node]]:
+        '''
+        Find Nodes at K distance
+        '''
+        if self.get_root() is not None: 
+            nodes_k:List[Node]=[]
+            nodes_k= self._nodes_k_distance(self.get_root(),nodes_k,k)
+            return nodes_k
+
+        else:
+            return None  
+
+
+    def _nodes_k_distance(self,node:Optional[Node],nodes_k:List[Node],k:int)->List[Node]:
+        if node is not None: 
+            if k == 0:
+                nodes_k.append(node)
+            self._nodes_k_distance(node.get_left_child(),nodes_k,k-1)
+            self._nodes_k_distance(node.get_right_child(),nodes_k,k-1) 
+            return nodes_k 
+        else:
+            return nodes_k 
+
+
+    def diameter_of_binary_tree(self,node:Optional[Node]=None)-> int: 
+        '''
+        Function to calculate diameter of binary tree
+        '''
+        if node is None:
+            return 0
+        else:
+            lheight:int = self._height_of_node(node.get_left_child())
+            rheight:int = self._height_of_node(node.get_right_child()) 
+
+            diameter_ltree:int = self.diameter_of_binary_tree(node.get_left_child())
+            diameter_rtree:int = self.diameter_of_binary_tree(node.get_right_child()) 
+
+            return max((lheight+rheight),max(diameter_ltree,diameter_rtree))  
+
+
+    def search_element_BST(self,key:int)->Optional[Node]: 
+        '''
+        Search for a key in BST
+        '''
+        return self._search_element_BST(key,self.get_root()) 
+
+
+    def _search_element_BST(self,key:int,node:Optional[Node])->Optional[Node]:
+        if node is None:
+            return None 
+        else: 
+            if key < node.get_key():
+                return self._search_element_BST(key,node.get_left_child()) 
+            elif key > node.get_key():
+                return self._search_element_BST(key,node.get_right_child())
+            else:
+                return node  
+
+
+    def print_ancestors_node(self,key:int,node:Optional[Node])->bool:
+        '''
+        Print ancestors of a key in binary_tree
+        '''
+        if node is None:
+            return False 
+        
+        elif node.get_key() == key :
+            return True
+
+        elif(self.print_ancestors_node(key,node.get_left_child()) or self.print_ancestors_node(key,node.get_right_child())):
+            print(node.get_key()) 
+            return True 
+        
+        else: return False 
+
+
+
+    def check_binary_tree_if_BST(self)-> bool: 
+        '''
+        check if a binary tree is BST without doing inorder traversing
+        '''
+        if self.get_root() is not None: 
+            MAX_INT = sys.maxsize
+            MIN_INT = -sys.maxsize
+            ans:bool = self._check_binary_tree_if_BST(self.get_root(),MAX_INT,MIN_INT)
+            return ans
+
+        else:
+            return False  
+
+    def _check_binary_tree_if_BST(self, node:Optional[Node],max:int,min:int)->bool: 
+
+        if node is None: return True
+
+        if(node.get_key() >= max or node.get_key() <= min):return False 
+
+        return( self._check_binary_tree_if_BST(node.get_left_child(),node.get_key(),min) and (self._check_binary_tree_if_BST(node.get_right_child(),node.get_key(),max))) 
+
+
+
+    def create_BST_from_sorted_array(self, arr:List[int])->None: 
+        '''
+        Create Binary Search Tree from sorted array
+        '''
+        self._create_BST_from_sorted_array(arr,0,len(arr)-1) 
+
+
+    def _create_BST_from_sorted_array(self,arr:List[int],min:int,max:int)-> None: 
+        if min > max: return
+
+        curr_key_index:int = int((min + max)/2)
+        self.insert(arr[curr_key_index]) 
+        self._create_BST_from_sorted_array(arr,min,curr_key_index-1) 
+        self._create_BST_from_sorted_array(arr,curr_key_index+1,max)  
+
+
+    def delete_node(self,key:int)->None:
+        """
+        Delete node of BST (Buggy Code)
+        Most difficult
+        """
+        node:Optional[Node] = self.search_element_BST(key) 
+        if node is not None:
+            # Case 1. Node is a leaf
+            if( node.get_left_child() is None and node.get_right_child() is None): 
+                if node.get_parent() is not None:
+                    if node.get_key() <= node.get_parent().get_key() : 
+                        node.get_parent().set_left_child(None)
+                    else:
+                        node.get_parent().set_right_child(None) 
+        
+
+            #Case 2. Node has both the child
+             #Find Inorder successor of Tree , copy the content to current Node and delete the inorder successor
+            elif  node.get_left_child() is not None and node.get_right_child() is not None :
+                inorder_succ_node:Optional[Node] = self.inorder_successor(node) 
+                if inorder_succ_node is not None: 
+                    tmp_key:int = inorder_succ_node.get_key()
+                    self.delete_node(tmp_key) 
+                    node.set_key(tmp_key) 
+
+            #case 3 Node has single child
+            else:
+                if node.get_right_child() is None and node.get_left_child() is not None:
+                    node.set_key(node.get_left_child().get_key())
+                    node.set_left_child(None) 
+                else:
+                    node.set_key(node.get_right_child().get_key())
+                    node.set_right_child(None)
+
+
+
+    
+
+
+
+
+
+
+
+    
+
+
+
+        
+
+
+
+
+
+
+        
+            
+
 
 
     
--- Python/Geek/binarytree/test_binary_tree.py
+++ Python/Geek/binarytree/test_binary_tree.py
@@ -1,6 +1,6 @@
 
 
-
+from __future__ import annotations
 import unittest
 from typing import List, Set, Dict, Tuple, Optional
 import random 
@@ -33,6 +33,7 @@ class BinaryTreeTestCase(unittest.TestCase):
         #checking elements are coming sorted after inorder traversal
         print('Elements after inorder traversal are :{}'.format(node_list_keys))
         self.assertEqual(elements,node_list_keys) 
+        print('******************************************\n')
 
 
     def test_inorder_successor(self):  
@@ -56,16 +57,177 @@ class BinaryTreeTestCase(unittest.TestCase):
 
         print('Inorder successor of each element in BST is:') 
         for i in range(len(node_list[:-1])):
-            in_succ_key:int = bt.inorder_successor(node_list[i])
-            print(node_list[i].get_key(),in_succ_key) 
-            self.assertEqual(in_succ_key,node_list[i+1].get_key())
-    
+            in_succ_node:Node = bt.inorder_successor(node_list[i])
+            print(node_list[i].get_key(),in_succ_node.get_key()) 
+            self.assertEqual(in_succ_node.get_key(),node_list[i+1].get_key()) 
+        print('******************************************\n') 
+
+
+    def test_height_of_tree(self): 
+        print('\nInserting Elements in BST using List') 
+        bt = BinaryTree()
+        elements :List[int] =[ ] 
+        random.seed(a=1)
+        elements = [random.randrange(0,101) for _ in range(10)]  
+        print('Elements are :{}'.format(elements))
+        for key in elements:
+            bt.insert(key) 
+
+        height : int = bt.height_of_tree() 
+        print('height of the tree is : {}'.format(height))
+        self.assertEqual(height,6)  
+        print('******************************************\n')
+
+
+    def test_level_order_traversal(self):
+        elements = [17, 72, 97, 8, 32, 15, 63, 97, 57, 60]  
+        print('Elements are :{}'.format(elements)) 
+        bt = BinaryTree()
+        print('\nInserting Elements in BST using List') 
+        for key in elements:
+            bt.insert(key)   
+        
+        print("Level Order Traversal is: ")
+        t:Tuple[Tuple[int,int],...]= bt.level_order_traversal()
+        correct_t: Tuple[Tuple[int,int],...] = ((1, 17), (2, 8), (2, 72), (3, 15), (3, 32), (3, 97), (4, 63), (4, 97), (5, 57), (6, 60)) 
+        self.assertEqual(t,correct_t) 
+        print('******************************************\n')
+
+
+    def test_width_of_tree(self): 
+        elements = [17, 72, 97, 8, 32, 15, 63, 97, 57, 60]  
+        print('Elements are :{}'.format(elements)) 
+        bt = BinaryTree()
+        print('\nInserting Elements in BST using List') 
+        for key in elements:
+            bt.insert(key)   
+        width:int = bt.width_of_tree() 
+        print('Width of the tree is :{}'.format(width))
+        self.assertEqual(width,3) 
+        print('******************************************\n')
+
+
+    def test_nodes_at_k_distance(self): 
+        elements = [17, 72, 97, 8, 32, 15, 63, 97, 57, 60]  
+        print('Elements are :{}'.format(elements)) 
+        bt = BinaryTree()
+        print('\nInserting Elements in BST using List') 
+        for key in elements:
+            bt.insert(key)   
+        nodes_k:List[Node] = bt.nodes_k_distance(2) 
+        nodes_k_keys = [nodes.get_key() for nodes in nodes_k]
+        print('Nodes at distance 2 are :{}'.format(nodes_k_keys))
+        self.assertEqual(nodes_k_keys,[15,32,97]) 
+        print('******************************************\n') 
+
+
+
+    def test_diameter_of_tree(self): 
+        elements = [17, 72, 97, 8, 32, 15, 63, 97, 57, 60]  
+        print('Elements are :{}'.format(elements)) 
+        bt = BinaryTree()
+        print('\nInserting Elements in BST using List') 
+        for key in elements:
+            bt.insert(key)   
+        diameter:int = bt.diameter_of_binary_tree(bt.get_root()) 
+        print('Diameter of the tree is:{}'.format(diameter))
+        self.assertEqual(diameter,7) 
+        print('******************************************\n') 
+
+
+    def test_search_key_in_BST(self): 
+        bt = BinaryTree()
+        elements :List[int] =[ ] 
+        elements = [random.randrange(0,101) for _ in range(10)]  
+        print('Elements are :{}'.format(elements))
+        for key in elements:
+            bt.insert(key)  
+
+        key:int = random.choice(elements)
+        print("Searching for following elemet in BST: {}".format(key))
+        node:Node = bt.search_element_BST(key) 
+        print("Following element found in BST: {}".format(node.get_key()))
+        self.assertEqual(node.get_key(),key) 
+        print('******************************************\n') 
+
+
+    def test_find_ancestors_in_BST(self): 
+        elements = [17, 72, 97, 8, 32, 15, 63, 97, 57, 60]  
+        print('Elements are :{}'.format(elements)) 
+        bt = BinaryTree()
+        print('\nInserting Elements in BST using List') 
+        for key in elements:
+            bt.insert(key)   
+
+        key:int = random.choice(elements)
+        print("Finding ancestors for following elemet in BST: {}".format(key))
+        element_present:bool = bt.print_ancestors_node(key,bt.get_root()) 
+        self.assertEqual(element_present,True) 
+        print('******************************************\n') 
+
+
+
+    def test_create_binary_search_tree_from_sorted_list(self):
+        bt = BinaryTree()
+        elements :List[int] = []
+        elements = [random.randrange(0,1000) for _ in range(20)]
+        elements.sort() 
+        print('Elements are {}'.format(elements)) 
+        bt.create_BST_from_sorted_array(elements)   
+
+        #Inoder Traversal
+        node_list:List[Node] = bt.inorder_tree_traversal()  
+        node_list_keys = [node.get_key() for node in node_list] 
+
+        #checking elements are coming sorted after inorder traversal
+        print('Elements after inorder traversal are :{}'.format(node_list_keys))
+        self.assertEqual(elements,node_list_keys) 
+        print('******************************************\n') 
+
+
+    def test_delete_node(self):
+        bt = BinaryTree()
+        elements :List[int] = [] 
+        #elements= [ random.randrange(0,1000) for _ in range(0,20)] 
+        elements =  [664, 120, 513, 233, 837, 117, 744, 289, 978, 488, 926, 799, 57, 168, 586, 937, 36, 821, 789, 7]
+        print('Elements are {}'.format(elements)) 
+        print('\nInserting Elements in BST using List') 
+        for key in elements:
+            bt.insert(key)   
+
+        #element_to_delete : int = random.choice(elements) 
+        element_to_delete : int = 744 
+        print("Element to be delete from BST:{}".format(element_to_delete))
+        bt.delete_node(element_to_delete) 
+
+        elements.remove(element_to_delete)
+        elements.sort() 
+        print('Initial Elements after sorting and removing key are {}'.format(elements))  
+        
+
+        #Inoder Traversal after deleting
+        node_list:List[Node] = bt.inorder_tree_traversal()  
+        node_list_keys = [node.get_key() for node in node_list] 
+
+        #checking elements are coming sorted after deletion of node
+        print('Elements after inorder traversal are :{}'.format(node_list_keys)) 
+        self.assertEqual(elements,node_list_keys) 
 
 
 
 
 if __name__ == "__main__": 
-    unittest.main()
+    #unittest.main()
+    test = BinaryTreeTestCase()
+    #test.test_height_of_tree()
+    #test.test_level_order_traversal()
+    #test.test_nodes_at_k_distance()
+    #test.test_diameter_of_tree()
+    #test.test_search_key_in_BST()
+    #test.test_find_ancestors_in_BST()
+    #test.test_create_binary_search_tree_from_sorted_list()
+    test.test_delete_node()
+
     
 
     
